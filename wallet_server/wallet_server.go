fd8c2d36c311b00f390fdadafced025bc27a6435
Alex <alexiavetere@gmail.com>
2022-03-30 12:23:59 -0300

    1.1.1 - wallet server

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+	"text/template"
+)
+
+// Ruta de templates:
+const tempDir = "wallet_server/templates"
+
+// Clase para el servidor de la wallet:
+type WalletServer struct {
+	port    uint16
+	gateway string
+}
+
+// Función para obtener el servidor de la wallet:
+func NewWalletServer(port uint16, gateway string) *WalletServer {
+	return &WalletServer{port, gateway}
+}
+
+// Método para obtener el puerto del servidor:
+func (ws *WalletServer) Port() uint16 {
+	return ws.port
+}
+
+// Método para obtener el gateway del servidor creado:
+func (ws *WalletServer) Gateway() string {
+	return ws.gateway
+}
+
+// Método para la redircción al template:
+func (ws *WalletServer) Index(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		t, _ := template.ParseFiles(path.Join(tempDir, "index.html"))
+		t.Execute(w, "")
+	default:
+		log.Printf("ERROR: Invalid HTTP Method")
+	}
+}
+
+// Método para correr el servidor:
+func (ws *WalletServer) Run() {
+	http.HandleFunc("/", ws.Index)
+	log.Fatal(http.ListenAndServe("localhost"+strconv.Itoa(int(ws.Port())), nil))
+}
